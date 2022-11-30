72c1335aa9f831df1f40fdef2eb6426c1a31225f
runnisha477 <75674285+runnisha477@users.noreply.github.com>
2022-11-30 10:21:57 +0700

    Add files via upload

---
+++ 4-20mA-loop.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 10:13:01 2022
+
+@author: asus
+"""
+
+#Thermistor inputs for 8 channels of temperature measurement
+#counts_to_value(reading_ad_counts, zero_ad_counts, span_ad_counts, zero_value, span_value )
+
+#where:
+
+#reading_ad_counts = actual A/D counts from the channel
+#zero_ad_counts = A/D counts to represent the Zero Value
+#span_ad_counts = A/D counts to represent the Span Value
+#zero_value = Zero Value ie: 0 mA or 4 mA
+#span_value = Span Value ie: 20 mA
+
+import time
+from datetime import datetime
+from time import sleep
+from widgetlords.pi_spi import *
+from widgetlords import *
+
+init()
+inputs = Mod8AI()
+
+Logging = 0
+
+while Logging < 1000:
+    A1 = inputs.read_single(0)
+    Temp = counts_to_value(A1, 745, 3723, 4, 20)   
+    file = open("Datalogger4-20mA.csv","a")
+    file.write("{0:0.2f}".format(Temp)+"mA"+",")
+    file.write(datetime.today().strftime('%Y-%m-%d'+"," '%H:%M:%S')+"\n")
+    sleep(1)
+    print(Temp)
+    Logging = Logging + 1
+    file.close()
